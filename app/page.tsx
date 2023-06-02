64d92f36137e7022f98d959bd76df917911cdc97
gh_masoud <msd.drg@gmail.com>
2023-06-03 01:31:08 +0330

    [ADD] api route handler for fetch some data from json file

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,14 @@
 import Link from "next/link";
 import React from "react";
+import Courses from "./components/Courses";
 
 function HomePage() {
   return (
-    <div>
+    <>
       <h1>Welcome to Traversy Media</h1>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/about/team">Team</Link>
-        </li>
-      </ul>
-    </div>
+      {/* @ts-expect-error Server Component */}
+      <Courses />
+    </>
   );
 }
 
